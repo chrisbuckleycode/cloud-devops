62418f6e81ca41dcdd8f25dfebff0046bb2f92f6
CB <62516125+chrisbuckleycode@users.noreply.github.com>
2024-09-10 23:19:19 +0800

    feat: k8s observability stack (#49)
    * feat: k8s observability stack
    
    * docs: update readme for k8s-observability-stack

---
+++ 2.shell/k8s-observability-stack/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+##
+## FILE: install.sh
+##
+## DESCRIPTION: Installs Prometheus, Grafana, Loki, Promtail to an existing Kubernetes cluster. Tested on a standard kind cluster
+##
+## AUTHOR: Chris Buckley (github.com/chrisbuckleycode)
+##
+## USAGE: install.sh
+##        
+##        When finished, do a port-forward on the prometheus-grafana service (or use ingress).
+##
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+kubectl create ns monitoring
+helm install loki grafana/loki --values values-loki.yaml --namespace monitoring
+helm install promtail grafana/promtail --values values-promtail.yaml --namespace monitoring
+helm install prometheus prometheus-community/kube-prometheus-stack --values values-grafana.yaml --namespace monitoring
+
+kubectl get po -n monitoring
