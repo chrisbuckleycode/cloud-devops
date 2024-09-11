d0ceb32102666b5cda6a942d37f89db3ec31038e
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2024-09-11 14:15:22 +0800

    fix: added crds to delete

--- 2.shell/k8s-observability-stack/uninstall.sh
+++ 2.shell/k8s-observability-stack/uninstall.sh
@@ -17,3 +17,14 @@ helm repo remove prometheus-community
 helm repo remove grafana
 
 kubectl delete ns monitoring
+
+kubectl delete crd scrapeconfigs.monitoring.coreos.com
+kubectl delete crd alertmanagerconfigs.monitoring.coreos.com
+kubectl delete crd prometheuses.monitoring.coreos.com
+kubectl delete crd prometheusagents.monitoring.coreos.com
+kubectl delete crd servicemonitors.monitoring.coreos.com
+kubectl delete crd probes.monitoring.coreos.com
+kubectl delete crd podmonitors.monitoring.coreos.com
+kubectl delete crd alertmanagers.monitoring.coreos.com
+kubectl delete crd thanosrulers.monitoring.coreos.com
+kubectl delete crd prometheusrules.monitoring.coreos.com
